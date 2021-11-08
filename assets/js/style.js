f4f0efd6e3357b1754d1c9dc59834b5342c07c51
Ario Pratomo <ariopratomo123@gmail.com>
2021-11-08 11:44:17 +0700

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "400px";
+    // document.getElementById("main").style.marginRight = "300px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    // document.getElementById("main").style.marginRight= "0";
+  }
